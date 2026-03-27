68405ffb167b39f2158861a9545ebbf5d4e5ea13
Kurisu <chrishunter2001@gmail.com>
2026-03-27 23:23:44 +0000

    Added load function to Config struct with error handling for missing file, also added craft workshop path function for finding steam workshop folder based on Stormworks directory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env::current_dir;
+use std::{env, fs};
 use std::fs::File;
-use std::io::BufWriter;
-use std::path::PathBuf;
+use std::io::{BufReader, BufWriter, Write};
+use std::path::{Path, PathBuf};
 use serde::{Deserialize, Serialize};
 use serde_json;
 
@@ -27,12 +28,55 @@ impl Config {
         let file = File::create("config.json");
         let mut writer = BufWriter::new(file.unwrap());
         serde_json::to_writer_pretty(&mut writer, &self).expect("File Write Failed!");
+        writer.flush().expect("Flush Failed!");
     }
+
+    fn load() -> Config {
+
+        if !Path::new("config.json").exists() {
+            let temp_conf = Config::new(env::current_dir().unwrap());
+            temp_conf.save();
+
+        }
+
+        let file = File::open("config.json");
+        let mut reader = BufReader::new(file.unwrap());
+        serde_json::from_reader(&mut reader).expect("JSON Parse Failed!")
+
+    }
+
+    fn validate_storm_dir(&self) -> bool {
+        let gamepath = self.storm_path.join("stormworks64.exe");
+        Path::new(&gamepath).exists()
+    }
+
+
+}
+
+fn craft_workshop_path(storm_path: PathBuf) -> PathBuf {
+    let mut path = storm_path;
+    path.pop();
+    path.pop();
+    path.push("workshop");
+    path.push("content");
+    path.push("573090");
+    return if path.exists() {
+        path
+    } else {
+        PathBuf::new()
+    }
+
 }
 
 fn main() {
-    let cwd = current_dir();
+    let mut config = Config::load();
 
-    let mut conf = Config::new(cwd.unwrap_or(PathBuf::new()));
-    conf.save();
+    if config.validate_storm_dir() {
+        println!("Valid");
+        config.storm_workshop_path = craft_workshop_path(config.storm_path);
+        println!("{}", config.storm_workshop_path.display());
+    }
+    else {
+        println!("Not valid");
+    }
 }
