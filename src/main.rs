e3cdc84dc24d7d2719c22c028b9eb55d5397dc15
Kurisu <chrishunter2001@gmail.com>
2026-03-27 22:17:52 +0000

    Implemented save function of Config struct and added gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,38 @@
 use std::env::current_dir;
+use std::fs::File;
+use std::io::BufWriter;
 use std::path::PathBuf;
+use serde::{Deserialize, Serialize};
+use serde_json;
 
-struct config {
+#[derive(Serialize, Deserialize)]
+struct Config {
     storm_path: PathBuf,
     storm_workshop_path: PathBuf,
     backup_path: PathBuf,
     output_path: PathBuf,
 }
 
-impl config {
-    fn new(current_dir: PathBuf) -> config {
-        config {
+impl Config {
+    fn new(current_dir: PathBuf) -> Config {
+        Config {
             storm_path: PathBuf::new(),
             storm_workshop_path: PathBuf::new(),
-            backup_path: current_dir.clone(),
-            output_path: current_dir,
+            backup_path: current_dir.join("backup"),
+            output_path: current_dir.join("output"),
         }
     }
 
     fn save(self) {
-        
+        let file = File::create("config.json");
+        let mut writer = BufWriter::new(file.unwrap());
+        serde_json::to_writer_pretty(&mut writer, &self).expect("File Write Failed!");
     }
 }
 
 fn main() {
     let cwd = current_dir();
 
-    let mut conf = config::new(cwd.unwrap_or(PathBuf::new()));
+    let mut conf = Config::new(cwd.unwrap_or(PathBuf::new()));
+    conf.save();
 }
